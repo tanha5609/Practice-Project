828f462bfcc8da7a796f90cd1e331077bb3b310b
tanha5609 <tanha15-5609.edu.bd>
2025-03-12 10:33:28 +0600

    secondary colors add

--- lib/utils/contants/colors.dart
+++ lib/utils/contants/colors.dart
@@ -6,7 +6,21 @@ class FColors{
   static const primaryNavyBlack = Color(0xFF0C1A30);
   static const primaryWhite = Color(0xFFFFFFFF);
 
+// Secondary
+  static const secondary = Color(0xFF3A9B7A);
+  static const secondaryRed = Color(0xFFC93545);
+  static const darkGrey = Color(0xFF838589);
+  static const halfGrey = Color(0xFFC4C5C4);
+  static const softGrey = Color(0xFFEDEDED);
+  static const offGrey = Color(0xFFFAFAFA);
 
+  // Label Colors
+  static const offBlue = Color(0xFFEFF5FB);
+  static const offGreen = Color(0xFFEEFAF6);
+  static const offRed = Color(0xFFFCECEF);
+
+  // Font Colors
+  static const oceanBlue = Color(0xFF3669C9);
   
   
 }
