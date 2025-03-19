478a472d0f908cc7a901bfd24d649096701ee786
tanha5609 <tanha15-5609.edu.bd>
2025-03-19 10:31:55 +0600

    boottom navigation

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -1,19 +1,73 @@
+
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lab7/common/widgets/app_bar.dart';
 
-import '../../../common/widgets/app_bar.dart';
-//import 'package:flutter_application_9/utils/constants/colors.dart';
-//import 'package:iconsax/iconsax.dart';
-
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    Container(
+      color: Colors.green,
+      height: double.infinity,
+    ),
+    Container(
+      color: Colors.redAccent,
+      height: double.infinity,
+    ),
+    Container(
+      color: Colors.blueAccent,
+      height: double.infinity,
+    ),
+    Container(
+      color: Colors.orangeAccent,
+      height: double.infinity,
+    ),
+  ];
+
+  void _onTapMethod(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: FAppBar(title: "Mega Shop", 
-      secondIcon: Iconsax.logout,),  
+      appBar: FAppBar(
+        title: "Mega Shop",
+        secondIcon: Icons.logout,
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTapMethod,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: "Wishlist",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: "Order",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Account",
+          ),
+        ],
+      ),
     );
   }
 }
